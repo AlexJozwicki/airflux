2de9494fc00cb9024c77c8c6db6853b1a49e7970
Alexandre Jozwicki <alexandre.jozwicki@kpmg.lu>
2015-04-13 15:55:55 +0200

    Adding another way of injecting stores

---
+++ src/FluxConnection.jsx
@@ -0,0 +1,27 @@
+var React   = require( 'react' );
+var FluxComponent   = require( './FluxComponent' );
+
+
+/**
+ * A component that is wired on Airflux or Reflux stores.
+ */
+class FluxConnection extends FluxComponent {
+    constructor( props ) {
+        super( props, props.stores || {} );
+    }
+
+    injectStores( component ) {
+        return React.addons.cloneWithProps( component, this.state );
+    }
+
+    render() {
+        if( React.Children.count( this.props.children ) === 0 ) {
+            return this.injectStores( React.Children.only( this.props.children ) );
+        }
+        else {
+            return <span>{ React.Children.map( this.props.children, ( child ) => this.injectStores( child ) ) }</span>;
+        }
+    }
+}
+
+module.exports = FluxConnection;
