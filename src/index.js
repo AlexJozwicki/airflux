1cd2d49873202c745c32d68e3702a9ad0e6fb4f0
Alexandre Jozwicki <contact@alexandrejozwicki.com>
2016-01-04 18:29:45 +0100

    Bad export; and if I export Listener right now, require/imports explode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export Action           from './Action';
 export SyncAction       from './SyncAction';
-export Listener         from './Action';
 export Publisher        from './Publisher';
 export Store            from './Store';
 export * as Joins       from './JoinStores';
