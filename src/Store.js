3e8c2fcc41bd059ef6facbad55f2a167c24f3953
Alexandre Jozwicki <contact@alexandrejozwicki.com>
2016-01-04 09:18:14 +0100

    Disable this for now

--- src/Store.js
+++ src/Store.js
@@ -6,17 +6,17 @@ export type StateMutation< State > = ( x: State ) => State;
 /**
  */
 export default class Store< State > extends Listener {
-    _state  : State;
+//    _state  : State;
 
     constructor() {
         super();
     }
 
     get eventType() : string { return 'change'; }
-
+/*
     get state() : State { return this._state; }
     set state( state: State ) { this._state = state; this.publishState(); }
-
+*/
     /**
      * Publishes the state to all subscribers.
      * This ensures that the stores always publishes the same data/signature.
