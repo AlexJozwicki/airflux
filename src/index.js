ae9414ea57179c0dcdf310b74017d1d7a0fabdc6
Alexandre Jozwicki <contact@alexandrejozwicki.com>
2016-08-25 11:27:08 +0200

    airflux 0.7.0-rc2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-export Action               from './Action';
-export AsyncResultAction    from './AsyncResultAction';
-export PromiseAction        from './PromiseAction';
-export SyncAction           from './SyncAction';
-export Publisher            from './Publisher';
-export Store                from './Store';
-export * as Joins           from './JoinStores';
-export FluxComponent        from './FluxComponent';
-export Capacitor            from './Capacitor';
+/* @flow */
+export { default as Action }                from './Action';
+export { default as AsyncResultAction }     from './AsyncResultAction';
+export { default as PromiseAction }         from './PromiseAction';
+export { default as SyncAction }            from './SyncAction';
+export { default as Publisher }             from './Publisher';
+export { default as Store }                 from './Store';
+export * as Joins                           from './JoinStores';
+export { default as FluxComponent }         from './FluxComponent';
+export { default as Capacitor }             from './Capacitor';
