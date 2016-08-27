3393e2fcaa6d10a99590f25c6a89f21298570f90
Alexandre Jozwicki <contact@alexandrejozwicki.com>
2016-08-27 16:48:33 +0200

    0.7.0. Still missing a good readme

--- es5/index.js
+++ es5/index.js
@@ -5,52 +5,82 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Capacitor = exports.FluxComponent = exports.Joins = exports.Store = exports.Publisher = exports.SyncAction = exports.PromiseAction = exports.AsyncResultAction = exports.Action = undefined;
 
-var _Action2 = require('./Action');
+var _Action = require('./Action');
 
-var _Action3 = _interopRequireDefault(_Action2);
+Object.defineProperty(exports, 'Action', {
+  enumerable: true,
+  get: function get() {
+    return _Action.default;
+  }
+});
 
-var _AsyncResultAction2 = require('./AsyncResultAction');
+var _AsyncResultAction = require('./AsyncResultAction');
 
-var _AsyncResultAction3 = _interopRequireDefault(_AsyncResultAction2);
+Object.defineProperty(exports, 'AsyncResultAction', {
+  enumerable: true,
+  get: function get() {
+    return _AsyncResultAction.default;
+  }
+});
 
-var _PromiseAction2 = require('./PromiseAction');
+var _PromiseAction = require('./PromiseAction');
 
-var _PromiseAction3 = _interopRequireDefault(_PromiseAction2);
+Object.defineProperty(exports, 'PromiseAction', {
+  enumerable: true,
+  get: function get() {
+    return _PromiseAction.default;
+  }
+});
 
-var _SyncAction2 = require('./SyncAction');
+var _SyncAction = require('./SyncAction');
 
-var _SyncAction3 = _interopRequireDefault(_SyncAction2);
+Object.defineProperty(exports, 'SyncAction', {
+  enumerable: true,
+  get: function get() {
+    return _SyncAction.default;
+  }
+});
 
-var _Publisher2 = require('./Publisher');
+var _Publisher = require('./Publisher');
 
-var _Publisher3 = _interopRequireDefault(_Publisher2);
+Object.defineProperty(exports, 'Publisher', {
+  enumerable: true,
+  get: function get() {
+    return _Publisher.default;
+  }
+});
 
-var _Store2 = require('./Store');
+var _Store = require('./Store');
 
-var _Store3 = _interopRequireDefault(_Store2);
+Object.defineProperty(exports, 'Store', {
+  enumerable: true,
+  get: function get() {
+    return _Store.default;
+  }
+});
+
+var _FluxComponent = require('./FluxComponent');
+
+Object.defineProperty(exports, 'FluxComponent', {
+  enumerable: true,
+  get: function get() {
+    return _FluxComponent.default;
+  }
+});
+
+var _Capacitor = require('./Capacitor');
+
+Object.defineProperty(exports, 'Capacitor', {
+  enumerable: true,
+  get: function get() {
+    return _Capacitor.default;
+  }
+});
 
 var _JoinStores = require('./JoinStores');
 
 var _Joins = _interopRequireWildcard(_JoinStores);
 
-var _FluxComponent2 = require('./FluxComponent');
-
-var _FluxComponent3 = _interopRequireDefault(_FluxComponent2);
-
-var _Capacitor2 = require('./Capacitor');
-
-var _Capacitor3 = _interopRequireDefault(_Capacitor2);
-
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.Action = _Action3.default;
-exports.AsyncResultAction = _AsyncResultAction3.default;
-exports.PromiseAction = _PromiseAction3.default;
-exports.SyncAction = _SyncAction3.default;
-exports.Publisher = _Publisher3.default;
-exports.Store = _Store3.default;
 exports.Joins = _Joins;
-exports.FluxComponent = _FluxComponent3.default;
-exports.Capacitor = _Capacitor3.default;
