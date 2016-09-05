751bc986282263cc1dd107f867f2eddd4e3d1e05
Alexandre Jozwicki <contact@alexandrejozwicki.com>
2016-09-05 10:27:47 +0200

    0.7.2

--- es5/Store.js
+++ es5/Store.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
@@ -45,14 +47,12 @@ var Store = (function (_Listener) {
         }
     }, {
         key: 'setState',
-        value: function setState() {
-            var transform = arguments.length <= 0 || arguments[0] === undefined ? null : arguments[0];
-
-            if (!!transform) {
-                this.state = transform(this.state);
+        value: function setState(partialState, callback) {
+            this.state = _extends({}, this.state, { partialState: partialState });
+            this.publishState();
+            if (typeof callback === 'function') {
+                callback();
             }
-
-            _get(Object.getPrototypeOf(Store.prototype), 'trigger', this).call(this, this.state);
         }
     }, {
         key: 'eventLabel',
